af4e6358f443ca679e7be3aa23c2b2a97f56dab5
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-25 02:07:10 +0200

    Update id.py

--- TelegraphBot/id.py
+++ TelegraphBot/id.py
@@ -1,1 +1,16 @@
+from Data import Data
+from pyrogram import Client, filters
 
+
+@Client.on_message(filters.private & filters.command(["id"]))
+async def id(bot, update):
+    text = STARTT_TEXT.format(update.from_user.id)
+    reply_markup = STARTT_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup,
+        Data.IDUSER,
+        quote=True
+        
+    )
