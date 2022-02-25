a9bccf95c8c0293be5a0de2e0aa51fe14376a7fe
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-25 02:44:03 +0200

    Update must_join.py

--- TelegraphBot/must_join.py
+++ TelegraphBot/must_join.py
@@ -13,15 +13,15 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+                link = "https://t.me/us7a5" + MUST_JOIN
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             await msg.reply(
-                f"You must join [this channel]({link}) to use me !",
+                f"You must join [this channel](https://t.me/us7a5) to use me !",
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                    InlineKeyboardButton("✨ Join Channel ✨", url=https://t.me/us7a5)
                 ])
             )
             await msg.stop_propagation()
