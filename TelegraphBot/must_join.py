8f095b83c781008df39714abc3db2672a3211757
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-25 02:49:53 +0200

    Update must_join.py

--- TelegraphBot/must_join.py
+++ TelegraphBot/must_join.py
@@ -22,7 +22,7 @@ async def must_join_channel(bot: Client, msg: Message):
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup([ 
             
-                 [InlineKeyboardButton("✨ Join Channel ✨", url=https://t.me/us7a5)]
+                   [InlineKeyboardButton("🖤🥀 𝑵𝑬𝑾 𝒀𝑶𝑹𝑲 🖤🥀", url="https://t.me/us7a5")]
 
                 ])
             )
