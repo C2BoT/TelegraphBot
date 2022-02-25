1422c5dfc370b3fa0eecd761625e8c0470d7919b
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-02-25 03:56:55 +0200

    Update callbacks.py

--- TelegraphBot/callbacks.py
+++ TelegraphBot/callbacks.py
@@ -33,13 +33,13 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
-    elif callback_query.data.lower() == "id":
+    elif callback_query.data.lower() == "help":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.message_id
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text=Data.USERID,
+            text=Data.HELP,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
